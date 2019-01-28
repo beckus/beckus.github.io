ddfed55d37e88560a4b41775c56a0d81f572c719
Andre Beckus <mikemail98-github@yahoo.com>
2019-01-28 00:54:17 -0500

    Change how spam is handled.

--- src/send_form_email.php
+++ src/send_form_email.php
@@ -8,7 +8,8 @@ if(isset($_POST['email'])) {
     $email_to = "contact@abeckus.com";
     $email_from = $email_to;
     $email_reply = $email_to;
-    $email_subject = "Email from abeckus.com contact form";
+    $email_good_subject = "Email from abeckus.com contact form";
+    $email_spam_subject = "SPAM EMAIL from abeckus.com contact form";
  
     function died($error) {
         // your error code can go here
@@ -38,24 +39,28 @@ if(isset($_POST['email'])) {
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
  
-  if(!preg_match($email_exp,$email_address)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
+    if(!preg_match($email_exp,$email_address)) {
+      $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+    }
  
     $string_exp = "/^[A-Za-z .'-]+$/";
 
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
+    if(strlen($error_message) > 0) {
+      died($error_message);
+    }
  
     $email_message = "Message is below.\n\n";
- 
-     
+      
     function clean_string($string) {
       $bad = array("content-type","bcc:","to:","cc:","href");
       return str_replace($bad,"<tokenremoved>",$string);
     }
  
+    if(empty($website)) {
+        $email_subject = $email_good_subject;
+    } else {
+        $email_subject = $email_spam_subject;
+    }
  
     $email_message .= "Name: ".clean_string($full_name)."\n";
     $email_message .= "Email: ".clean_string($email_address)."\n";
